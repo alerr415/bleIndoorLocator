30342394e3e7ddf7ad18a8a5d53f31375448cc66
Attila772 <71554727+Attila772@users.noreply.github.com>
2021-01-29 12:42:37 +0100

    Config on server

--- Configuration.py
+++ Configuration.py
@@ -1,21 +1,21 @@
 def configurator(var):
 
-    if var[0] == 1: # mernok1
+    if var[0] == '1': # mernok1
         var[1] = var[1]
 
-    if var[0] == 2:  # ebedlo
+    if var[0] == '2':  # ebedlo
         var[1] = var[1]
 
-    if var[0] == 3:  # mernok2
+    if var[0] == '3':  # mernok2
         var[1] = var[1]
 
-    if var[0] == 4:  # kistargyalo
+    if var[0] == '4':  # kstargyalo
         var[1] = var[1] + 20
 
-    if var[0] == 5:  # nagytarghyalo
+    if var[0] == '5':  # nagytarghyalo
         var[1] = var[1] + 10
 
-    if var[0] == 6:  # iroda
-        var[1] = var[1] + 10
+    if var[0] == '6':  # iroda
+        var[1] = var[1] + 15
 
     return var
